4c3e526912f6d0bfbbed6014e829f8dc0c2bb874
amchampoux <amchampoux@mirego.com>
2023-02-02 14:05:16 -0500

    add quizzes, questions and quiz_attempts data seeds

--- db/seeds/02_quizzes.sql
+++ db/seeds/02_quizzes.sql
@@ -1,7 +1,7 @@
-INSERT INTO quizzes (owner_id, title, description, privacy, total_questions)
+INSERT INTO quizzes (owner_id, title, description, public, total_questions)
 
 VALUES (1, 'Star Wars Trivia', 'Do you really know Star Wars? Are you a real fan? Discover how much you are into it with this quiz.', FALSE, 3),
 (2, 'The Love Test', '​People do grand things for love. King Edward VIII gave up the throne and Shah Jahan built the Taj Mahal. What would you do for love? Find out with this test!', TRUE, 3),
 (1, 'Pandemic resilience test', 'The COVID-19 pandemic has proven to be one of the greatest health and economic challenges we have faced on a global scale. The purpose of this assessment is to evaluate your current mental state.', FALSE, 3),
 (2, 'The President test', 'It would be so cool to have a huge office, personal plane, but being president comes with major responsibilities. Think you have what it takes to run a country? Before you get sworn in, take this test!', FALSE, 3),
-(1, 'Canadian food test', "It takes a whole lot of hearty food, plus plenty of carbs and coffee from Tim Hortons ... Take our quiz to see how much you know about Canada's favorites foods!", FALSE, 3)
+(1, 'Canadian food test', 'It takes a whole lot of hearty food, plus plenty of carbs and coffee from Tim Hortons ... Take our quiz to see how much you know about Canada favorites foods!', FALSE, 3)
