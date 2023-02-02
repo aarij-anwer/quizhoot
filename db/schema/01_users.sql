8166d0ced27ebcfb991876c2d7441580bca51bb4
amchampoux <amchampoux@mirego.com>
2023-02-02 13:33:17 -0500

    create DB, tables and users seed

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -5,5 +5,5 @@ CREATE TABLE users (
   name VARCHAR(255) NOT NULL,
   email VARCHAR(255) NOT NULL,
   password VARCHAR(255) NOT NULL,
-  created_at TIMESTAMP NOT NULL
+  created_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
