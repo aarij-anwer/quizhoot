07160978e5b608e1e808e5807daece9e56c62f3a
Rebecca Kurtis <rebecca.kurtis@hotmail.com>
2023-02-02 09:19:55 -0800

    Add blank seed files

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -3,8 +3,3 @@ VALUES ('Aarij Anwer', 'aarij.anwer@gmail.com', 'password'),
 ('Atiqa Ahmed', 'atiqa426@gmail.com', 'password'),
 ('Nadira Moyez', 'nadiramoyez@gmail.com', 'password');
 
-INSERT INTO quizzes (owner_id, title, description, privacy, total_questions)
-VALUES (1, 'My first quiz', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.', TRUE, 10),
-(2, 'My first quiz', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.', TRUE, 10),
-(1, 'My private quiz', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.', FALES, 5);
-
---
+++ db/seeds/02_quizzes.sql
@@ -0,0 +1,4 @@
+INSERT INTO quizzes (owner_id, title, description, privacy, total_questions)
+VALUES (1, 'My first quiz', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.', TRUE, 10),
+(2, 'My first quiz', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.', TRUE, 10),
+(1, 'My private quiz', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.', FALES, 5);
