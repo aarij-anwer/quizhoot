fe6402479bf35cd25521e17eacbd212caa115cb8
amchampoux <amchampoux@mirego.com>
2023-02-03 13:05:04 -0500

    create addquiz db helper and POST api

--- db/queries/database.js
+++ db/queries/database.js
@@ -1,1 +1,32 @@
+const db = require('../connection');
+
+const createNewQuiz = function(quiz) {
+  return db
+    .query(`INSERT INTO quizzes(owner_id, title, description, public) VALUES($1, $2, $3, $4) RETURNING *`,
+      [quiz.owner_id, quiz.title, quiz.description,quiz.public])
+    .then(data => {
+      return data.rows;
+    });
+
+};
+
+module.exports = { createNewQuiz };
+// CREATE TABLE quizzes (
+//   id SERIAL PRIMARY KEY NOT NULL,
+//   owner_id INTEGER REFERENCES users(id),
+//   title VARCHAR(255) NOT NULL,
+//   description TEXT NOT NULL,
+//   public BOOLEAN NOT NULL,
+//   Total_questions INTEGER
+// );
+// CREATE TABLE questions (
+//   id SERIAL PRIMARY KEY NOT NULL,
+//   quiz_id INTEGER REFERENCES quizzes(id),
+//   question_text VARCHAR(255) NOT NULL,
+//   option_1 VARCHAR(255) NOT NULL,
+//   option_2 VARCHAR(255) NOT NULL,
+//   option_3 VARCHAR(255) NOT NULL,
+//   correct_answer VARCHAR(255) NOT NULL
+// );
+
 
--- routes/create-quiz.js
+++ routes/create-quiz.js
@@ -1,16 +1,32 @@
+/* eslint-disable camelcase */
 const express = require('express');
+const db = require('../db/connection');
 const router  = express.Router();
+
 
 router.get('/new', (req, res) => {
   res.render('create-quiz');
-  console.log("hello new quiz");
 });
 
 
 
+router.post('/new', (req, res) => {
+  // Once login api added, owner_id needs to = userId
+  // const userId = req.session.userId;
+  console.log(req.body);
+  db.createNewQuiz({...req.body, owner_id : 1})
 
-router.post('/quizzes', (req, res) => {
-  res.render('quizzes');
+    .then(quiz => {
+      res.send(quiz);
+      res.redirect('/');
+    })
+    .catch(e => {
+      console.error(e);
+      res.send(e);
+    });
+
+  return router;
+
 });
 
 
