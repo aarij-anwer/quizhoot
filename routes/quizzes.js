9a7cb54326b717965fdb690fb413288147f20348
Rebecca Kurtis <rebecca.kurtis@hotmail.com>
2023-02-05 14:07:10 -0800

    Remove commented code and debug logs

--- routes/quizzes.js
+++ routes/quizzes.js
@@ -7,36 +7,9 @@ const router  = express.Router();
 router.get('/home', (req, res) => {
   db.getAllQuizzes(10)
     .then(data => {
-      console.log('data', data);
-      console.log('data 0', data[0]);
-      // const result = {};
-      // for (let i = 0; i < data.length; i++) {
-      //   result[i] = data[i];
-      // }
-      // // return result;
-      // console.log('result', result);
-
-      // for (const obj in result) {
-      //   console.log('obj', result[obj]);
-      //   // const templateVars = {
-      //   //   obj
-      //   // };
-      //   // res.render('home-page', templateVars);
-      // }
-      // const arrToObject = arr => {
-      //   const result = {};
-      //   for (let i = 0; i < arr.length; i++) {
-      //     // const key = arr[i]['id'];
-      //     result[i] = arr[i];
-      //   }
-      //   return result;
-      // };
-      // const obj = arrToObject(data);
       const templateVars = {
         quizzes: data
       };
-      console.log('templateVars', templateVars);
-      // res.render('home-page', result);
       res.render('home-page', templateVars);
     })
     .catch(e => {
