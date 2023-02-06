a921997634b4585970cf828f066977ec970a7698
Aarij Anwer <aarij.anwer@gmail.com>
2023-02-06 03:32:38 -0500

    add a addUser function to db/queries/users.js and use it in the registration route

--- routes/users.js
+++ routes/users.js
@@ -8,16 +8,53 @@
 
 const express = require('express');
 const router  = express.Router();
+const db = require('../db/queries/users');
 
-router.post('/', (req, res) => {
-  console.log("Post request");
+router.get('/', (req, res) => {
   res.redirect('/quizzes/home');
 });
 
-//user registration
+//user registration routes
 router.get('/new', (req, res) => {
-  console.log("User registration");
   res.render('register');
 });
+
+router.post('/', (req, res) => {
+  const name = req.body.name;
+  const email = req.body.email;
+  const password = req.body.password;
+  const newUser = {
+    name,
+    email,
+    password
+  };
+
+  if ((!name) || (!email) || (!password)) {
+    //no name and/or email and/or password provided
+    return res.status(400).send(`<p>Please enter a name, email and password!</p><button onclick="history.back()">Go Back</button>`);
+  }
+
+  // db.getUser(email)
+  //   .then(user => {
+  //     if (user) {
+  //       return res.status(400).send(`<p>User exists!</p><button onclick="history.back()">Go Back</button>`);
+  //     }
+  //   })
+  //   .catch(e => res.send(e));
+
+
+  db.addUser(newUser)
+    .then(addedUser => {
+      if (!addedUser) {
+        return res.status(400).send(`<p>Something went wrong with the DB`);
+      }
+      console.log(addedUser);
+      res.cookie('user_id', addedUser.id);
+      res.redirect('/quizzes/home');
+    })
+    .catch(e => res.send(e));
+});
+
+
 
 module.exports = router;
